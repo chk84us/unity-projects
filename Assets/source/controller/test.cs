e4f9e989e6f670ea1f575a001bfd16927a96bbf2
Harish Kumar Chakravarthy <harishkumar.chakravarthy@booxware.de>
2016-04-09 17:50:40 +0200

    Some more refactoring

--- Assets/source/controller/test.cs
+++ Assets/source/controller/test.cs
@@ -1,14 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using utils;
+using System.IO;
 
 public class test : MonoBehaviour {
+
+	private const string fileName = "goals.json";
+	private const string dataFolderName = "data";
+	string[] paths;
 
 	// Use this for initialization
 	void Start () {
 		JsonIo io = new JsonIo ();
-		io.SerializeData ();
-	
+		setupFilePaths ();
+		GoalListWrapperModel goalsListWrapper = new GoalListWrapperModel();
+		List<GoalModel> goalList = new List<GoalModel> ();
+		goalList.Add (new GoalModel (1, "Test goal 1", 10, 3, false));
+		goalList.Add (new GoalModel (2, "Test goal 2", 1, 1, false));
+		goalList.Add (new GoalModel (3, "Test goal 3", 5, 0, false));
+		goalsListWrapper.goalList = goalList;
+		string fullFilePath = string.Join(Path.DirectorySeparatorChar.ToString(), paths);
+		io.SerializeData(goalsListWrapper, fullFilePath);
+	}
+
+	private void setupFilePaths() {
+		paths = new string[3];
+		paths [0] = Application.dataPath;
+		paths [1] = dataFolderName;
+		paths [2] = fileName;
 	}
 	
 	// Update is called once per frame
